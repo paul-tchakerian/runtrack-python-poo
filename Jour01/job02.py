dc7ecbfdb6030217c390da1724f2727b834fa330
paul-tchakerian <115216830+paul-tchakerian@users.noreply.github.com>
2023-02-27 13:24:23 +0100

    Add files via upload

---
+++ Jour01/job02.py
@@ -0,0 +1,10 @@
+class Operation:
+    def __init__( self, nombre1, nombre2 ):
+            self.nombre1 = nombre1
+            self.nombre2 = nombre2
+
+Operation.nombre1 = 45
+Operation.nombre2 = 124
+
+print(f'Le nombre1 est {Operation.nombre1}')
+print(f'Le nombre2 est {Operation.nombre2}')
